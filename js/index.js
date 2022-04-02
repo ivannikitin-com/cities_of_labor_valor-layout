36cc6f3f4603eb5791201621e08cee1249dd6f72
Груздева Светлана <gruzdeva.s@outlook.com>
2022-04-02 13:20:54 +0300

    Настроено мобильное меню

--- js/index.js
+++ js/index.js
@@ -4,9 +4,12 @@ const navToggle = document.querySelector(".navbar-toggler");
 
 navToggle.addEventListener("click", () => {
 	const visibility = primaryNav.getAttribute("data-visible");
+
 	if (visibility === "false") {
 		primaryNav.setAttribute("data-visible", true);
+		navToggle.setAttribute("aria-expanded", true);
 	} else if (visibility === "true") {
 		primaryNav.setAttribute("data-visible", false);
+		navToggle.setAttribute("aria-expanded", false);
 	}
 });
