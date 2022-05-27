2821bcd3ecb2ef7be91179794b6c955d1354d0d8
Груздева Светлана <gruzdeva.s@outlook.com>
2022-05-27 19:16:10 +0300

    Добавлен блок с заголовком для города

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-'use strict'
+"use strict";
 const primaryNav = document.querySelector(".header__collapse");
 const navToggle = document.querySelector(".navbar-toggler");
 
 navToggle.addEventListener("click", () => {
-	const visibility = primaryNav.getAttribute("data-visible");
+  const visibility = primaryNav.getAttribute("data-visible");
 
-	if (visibility === "false") {
-		primaryNav.setAttribute("data-visible", true);
-		navToggle.setAttribute("aria-expanded", true);
-	} else if (visibility === "true") {
-		primaryNav.setAttribute("data-visible", false);
-		navToggle.setAttribute("aria-expanded", false);
-	}
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
 });
 
-// let countColor = document.querySelector('.search-results__link').closest("search-results__item::before");
-
-// countColor.addEventListener('mouseover', () => {
-//     countColor.style.color = `#green`;
-// });
-
-// countColor.addEventListener('mouseout', () => {
-//     countColor.style.color = "#000";
-// });
+var items = document.querySelectorAll(".search-results__item");
+items.forEach(function (item) {
+  item.addEventListener("mouseover", function hover() {
+    item.querySelector(".search-results__serial-num").style.color =
+      "var(--red-100)";
+  });
+  item.addEventListener("mouseleave", function leave() {
+    item.querySelector(".search-results__serial-num").style.color =
+      "var(--blue)";
+  });
+});
